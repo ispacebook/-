852a974e05ab382a8295df26d0202f95506eac56
ispacebook <84063822+ispacebook@users.noreply.github.com>
2021-07-05 19:25:26 +0800

    Add files via upload
    数据可视化

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask,render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/index')
+def home():
+    return index()
+
+@app.route('/movie')
+def movie():
+    datalist = []
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select * from movie250"
+    data = cur.execute(sql)
+    for items in data:
+        datalist.append(items)
+    cur.close()
+    con.close()
+    return render_template('/movie.html',movies = datalist)
+
+@app.route('/word')
+def word():
+    return render_template('/word.html')
+
+@app.route('/team')
+def team():
+    return render_template('/team.html')
+
+@app.route('/score')
+def score():
+    score = []
+    num = []
+    con = sqlite3.connect("movie.db")
+    cur = con.cursor()
+    sql = "select score,count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for items in data:
+        score.append(items[0])
+        num.append(items[1])
+    cur.close()
+    con.close()
+    return render_template('/score.html',score=score,num=num)
+
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/testCould.py
@@ -0,0 +1,19 @@
+#-*- coding = utf-8 -*-
+'''
+
+Time : 2021/7/3 10:11
+File : testCould.py
+USER : liuhp
+'''
+
+import jieba
+from matplotlib import pyplot as plt
+from wordcloud import wordcloud
+from PIL import Image
+import numpy as np
+import pymysql
+
+
+
+
+
